68769eb083d43495f49f3720d4360639e0ff1b01
Sneha Supriya <codingsneha@gmail.com>
2024-03-18 22:54:44 +0530

    Add GBM Model

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
 from sklearn.preprocessing import LabelEncoder
-from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 from sklearn.naive_bayes import GaussianNB
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+from sklearn.metrics import accuracy_score
 from collections import Counter
 
 # Read the dataset
@@ -28,11 +26,13 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 final_svm_model = SVC()
 final_nb_model = GaussianNB()
 final_rf_model = RandomForestClassifier(random_state=18)
+final_gbm_model = GradientBoostingClassifier(random_state=42)  # Add GBM model
 
 # Train the models
-final_svm_model.fit(X, y)
-final_nb_model.fit(X, y)
-final_rf_model.fit(X, y)
+final_svm_model.fit(X_train, y_train)
+final_nb_model.fit(X_train, y_train)
+final_rf_model.fit(X_train, y_train)
+final_gbm_model.fit(X_train, y_train)  # Train GBM model
 
 # Create a symptom index dictionary
 symptoms = X.columns.values
@@ -57,13 +57,15 @@ def predictDisease(symptoms):
     rf_prediction = encoder.inverse_transform([final_rf_model.predict(input_data)])[0]
     nb_prediction = encoder.inverse_transform([final_nb_model.predict(input_data)])[0]
     svm_prediction = encoder.inverse_transform([final_svm_model.predict(input_data)])[0]
+    gbm_prediction = encoder.inverse_transform([final_gbm_model.predict(input_data)])[0]  # Predict using GBM
 
-    final_prediction = mode_str([rf_prediction, nb_prediction, svm_prediction])[0]
+    final_prediction = mode_str([rf_prediction, nb_prediction, svm_prediction, gbm_prediction])[0]
 
     predictions = {
         "rf_model_prediction": rf_prediction,
         "naive_bayes_prediction": nb_prediction,
         "svm_model_prediction": svm_prediction,
+        "gbm_model_prediction": gbm_prediction,  # Include GBM prediction
         "final_prediction": final_prediction
     }
     return predictions
@@ -78,6 +80,7 @@ def main():
         st.write(f"RF Model Prediction: {predictions['rf_model_prediction']}")
         st.write(f"Naive Bayes Model Prediction: {predictions['naive_bayes_prediction']}")
         st.write(f"SVM Model Prediction: {predictions['svm_model_prediction']}")
+        st.write(f"GBM Model Prediction: {predictions['gbm_model_prediction']}")  # Display GBM prediction
         st.write(f"Final Prediction: {predictions['final_prediction']}")
 
 if __name__ == "__main__":
